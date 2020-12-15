9648b5b259e20766792f91ba37110c6d592b756e
Ric Allinson <ric@ricallinson.com>
2020-12-15 12:20:34 -0800

    Added maps for intake and exhaust

--- ArduinoFreevalve/ArduinoFreevalve.ino
+++ ArduinoFreevalve/ArduinoFreevalve.ino
@@ -3,17 +3,16 @@
 // Wesley Kagan, 2020
 // Ric Allinson, 2020
 
-// Control variables.
-const int DEG_PER_MAGNET = 6;       // Number of degrees for each magnet.
-const int FREEVALVE_OFFSET_TOP = 0; // The amount of off set from TDC (needs to be multiples of DEG_PER_MAGNET).
-const int FREEVALVE_OFFSET_BOTTOM = 180 + FREEVALVE_OFFSET_TOP; 
-
 // Pins assignment.
 const int HALL_MAGNET = 2;
 const int EXHAUST_V = 12;
 const int INTAKE_V = 13;
 
-int cad;                   // Crank Angle Degrees (CAD).
+// Control variables.
+const int DEG_PER_MAGNET = 6;    // Number of degrees for each magnet.
+const bool INTAKE_MAP[120] = {};  // Intake open/close mapping from -360 to 360.
+const bool EXHAUST_MAP[120] = {}; // Exhaust open/close mapping from -360 to 360.
+
 int hallCounter;           // The number of magnets after the last TDC.
 bool cycle;                // "true" for Intake, "false" for Exhaust.
 bool printLog;             // Used to stop duplicate values from printing in the loop.
@@ -31,8 +30,8 @@ void setup() {
 void loop() {
   if(printLog){
     Serial.print(lastTimeGap);
+    Serial.print(hallCounter);
     Serial.print(cycle);
-    Serial.print(cad);
     printLog = false;
   }
 }
@@ -44,10 +43,12 @@ void magnetDetect() {
   // Get the time gap between this interrupt and the last.
   timeGap = millis() - lastTimeGap;
 
-  // Find missing tooth for Top Dead Center (TDC).
+  // Find the missing tooth for Top Dead Center (TDC).
   if (timeGap >= lastTimeGap * 3 / 2) {
-    // Reset the hall counter.
-    hallCounter = 1;
+    // On the second rotation reset the hall counter.
+    if (cycle) {
+      hallCounter = 0;
+    }
     // Flip the cycle phase.
     cycle = !cycle;
   }
@@ -55,16 +56,9 @@ void magnetDetect() {
   // Store the last time difference so we can use it in the next interrupt.
   lastTimeGap = timeGap;
 
-  // Store the current crank angle for logging.
-  cad = hallCounter * DEG_PER_MAGNET;
+  // Use the intake/exhaust maps to open or close the valves.
+  digitalWrite(INTAKE_V, INTAKE_MAP[hallCounter]);
+  digitalWrite(EXHAUST_V, EXHAUST_MAP[hallCounter]);
 
-  // Every rotation of the crank alternates between Intake and Exhaust.
-  if (cycle) {
-    // If the crank angle degree is in the intake range open it, otherwise close it.
-    digitalWrite(INTAKE_V, cad > FREEVALVE_OFFSET_TOP && cad <= FREEVALVE_OFFSET_BOTTOM);
-  } else {
-    // If the crank angle degree is in the exhaust range open it, otherwise close it.
-    digitalWrite(EXHAUST_V, cad >= FREEVALVE_OFFSET_BOTTOM || cad < FREEVALVE_OFFSET_TOP);
-  }
   printLog = true;
 }
