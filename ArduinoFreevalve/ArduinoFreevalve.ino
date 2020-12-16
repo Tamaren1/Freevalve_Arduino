30341de9497ab3538cd51a2626f42a8aeeb504df
Ric Allinson <ric@ricallinson.com>
2020-12-16 15:30:43 -0800

    Addressed code comments for bad maths.

--- ArduinoFreevalve/ArduinoFreevalve.ino
+++ ArduinoFreevalve/ArduinoFreevalve.ino
@@ -12,8 +12,8 @@ const int INTAKE_V = 13;
 
 // Control constants.
 const int DEG_PER_MAGNET = 6; // Number of degrees for per magnet.
-const int ROTATION_TWO = 760/DEG_PER_MAGNET;     // Number of interrupts in the full cycle.
-const int ROTATION_ONE = (760/DEG_PER_MAGNET)-1; // Number of interrupts in a half cycle.
+const int ROTATION_TWO = (720/DEG_PER_MAGNET)-1;   // Number of interrupts in the full cycle.
+const int ROTATION_ONE = (720/DEG_PER_MAGNET)-2; // Number of interrupts in a half cycle.
 
 // Runtime mapping variables.
 bool intakeMap[ROTATION_TWO] = {};  // Intake open/close mapping is equal the total number of interrupts for two rotations.
@@ -42,17 +42,25 @@ void loop() {
   if(printLog){
     Serial.print(lastTimeGap);
     Serial.print(hallCounter);
-    Serial.print(secondRotation);
+    Serial.println(secondRotation);
     printLog = false;
   }
 }
 
-// This function will be called about every 15.79ms at the maximum RPM of 3800.
+// This function will be called about every ~263.16usec at the maximum RPM of 3800.
+// The missing tooth will come by roughly every 15.79 ms.
+// (1 rev / 60 pulses) * (1 min / 3800 rev) * (60 sec / min) = 1 / 3800 sec/pulse = ~263.16usec.
 void magnetDetect() {
   // Increment the counter to keep track of the position.
   hallCounter++;
+
   // Get the time gap between this interrupt and the last.
-  timeGap = millis() - lastTimeGap;
+  // Returns the number of microseconds since the Arduino board began running the current program.
+  // This number will overflow (go back to zero), after approximately 70 minutes.
+  // On 16 MHz Arduino boards (e.g. Duemilanove and Nano),
+  // this function has a resolution of four microseconds (i.e. the value returned is always a multiple of four).
+  // On 8 MHz Arduino boards (e.g. the LilyPad), this function has a resolution of eight microseconds.
+  timeGap = micros() - lastTimeGap;
 
   // Find the missing tooth for Top Dead Center (TDC).
   if (timeGap >= lastTimeGap * 3 / 2) {
