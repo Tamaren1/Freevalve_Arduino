a20a930aa3c70f5fd8f50e5176077736cd177f25
Ric Allinson <ric@ricallinson.com>
2020-12-17 10:58:36 -0800

    Update to reduce computation time ISR

--- ArduinoFreevalve/ArduinoFreevalve.ino
+++ ArduinoFreevalve/ArduinoFreevalve.ino
@@ -73,7 +73,7 @@ void magnetDetect() {
   timeGap = micros() - lastTimeGap;
 
   // Detect the missing step.
-  if (timeGap >= lastTimeGap + lastTimeGap / 2) {
+  if (timeGap * 10 >= lastTimeGap * 15) {
     // On the second cycle reset the hallCounter.
     if (secondCycle) {
       hallCounter = 0;
