875770332a89fc9ab0b8f92d2c2c24f0b487c7e0
Ric Allinson <ric@ricallinson.com>
2020-12-18 14:05:21 -0800

    Removed timing code and added a second pulse for rotation detection.

--- ArduinoFreevalve/ArduinoFreevalve.ino
+++ ArduinoFreevalve/ArduinoFreevalve.ino
@@ -7,17 +7,16 @@
 
 // Pins assignment.
 static const int HALL_MAGNET = 2;
+static const int ROTATION_MAGNET = 3;
 static const int EXHAUST_V = 12;
 static const int INTAKE_V = 13;
 
 // Implementation constants (to be changed by implementer).
 static const int NUM_STEPS = 60;
-static const int NUM_MISSING_STEPS = 1;
 
 // Control constants.
-static const int NUM_TRIGGER_STEPS = NUM_STEPS - NUM_MISSING_STEPS; // Number of interrupt steps counted.
-static const int STEPS_PER_ROTATION = NUM_TRIGGER_STEPS;            // Number of interrupts in a half cycle.
-static const int STEPS_PER_CYCLE = NUM_TRIGGER_STEPS * 2;           // Number of interrupts in the full cycle.
+static const int STEPS_PER_ROTATION = NUM_STEPS;            // Number of interrupts in a half cycle.
+static const int STEPS_PER_CYCLE = NUM_STEPS * 2;           // Number of interrupts in the full cycle.
 
 // Runtime mapping variables.
 bool intakeMap[STEPS_PER_CYCLE] = {};  // Intake open/close mapping is equal the total number of interrupts for two rotations.
@@ -27,13 +26,12 @@ bool exhaustMap[STEPS_PER_CYCLE] = {}; // Exhaust open/close mapping is equal th
 volatile int hallCounter = 0;           // The number of magnets after the last TDC.
 volatile bool secondCycle = false;   // "true" if the cam is on its second rotation.
 volatile bool printLog = false;         // Used to stop duplicate values from printing in the loop.
-volatile unsigned long timeGap = 0;     // Function level time between interrupts.
-volatile unsigned long lastTimeGap = 0; // Global level time between interrupts.
 
 void setup() {
   Serial.begin(115200);
   pinMode(HALL_MAGNET, INPUT);
-  attachInterrupt(0, magnetDetect, RISING);
+  attachInterrupt(0, rotationDetect, RISING);
+  attachInterrupt(1, magnetDetect, RISING);
   pinMode(INTAKE_V, OUTPUT);
   pinMode(EXHAUST_V, OUTPUT);
   /* 
@@ -46,44 +44,29 @@ void setup() {
 void loop() {
   // This may not print all values. Only the most recent from the last interrupt.
   if(printLog){
-    Serial.print(lastTimeGap);
     Serial.print(hallCounter);
     Serial.println(secondCycle);
     printLog = false;
   }
 }
 
+// This function is called once per rotation.
+void rotationDetect() {
+  if (secondCycle) {
+    hallCounter = 0;                  // On the second cycle reset the hallCounter.
+  } else {
+    hallCounter = STEPS_PER_ROTATION; // Forcing the counter in case of drift over a rotation.
+  }
+  // Flip the secondCycle flag.
+  secondCycle = !secondCycle;
+}
+
+// This function is called NUM_STEPS per rotation.
 // This function will be called about every ~263.16usec at the maximum RPM of 3800.
-// The missing tooth will come by roughly every 15.79 ms.
 // (1 rev / 60 pulses) * (1 min / 3800 rev) * (60 sec / min) = 1 / 3800 sec/pulse = ~263.16usec.
 void magnetDetect() {
   // Increment the counter to keep track of the position.
   hallCounter++;
-
-  // Get the time gap between this interrupt and the last.
-  // 
-  // The following will be a problem...
-  //
-  // Returns the number of microseconds since the Arduino board began running the current program.
-  // This number will overflow (go back to zero), after approximately 70 minutes.
-  // On 16 MHz Arduino boards (e.g. Duemilanove and Nano),
-  // this function has a resolution of four microseconds (i.e. the value returned is always a multiple of four).
-  // On 8 MHz Arduino boards (e.g. the LilyPad), this function has a resolution of eight microseconds.
-  timeGap = micros() - lastTimeGap;
-
-  // Detect the missing step.
-  if (timeGap * 10 >= lastTimeGap * 15) {
-    if (secondCycle) {
-      hallCounter = 0;                  // On the second cycle reset the hallCounter.
-    } else {
-      hallCounter = STEPS_PER_ROTATION; // Forcing the counter in case of drift over a rotation.
-    }
-    // Flip the secondCycle flag.
-    secondCycle = !secondCycle;
-  }
-
-  // Store the last time difference so we can use it in the next interrupt.
-  lastTimeGap = timeGap;
 
   // If the hallCounter is greater than the mapping index reset it.
   // This would only happen when the missing tooth was NOT detected.
